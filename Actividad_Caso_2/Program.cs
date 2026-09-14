6c75f816d19d40b75b8ac6c35af21bf24f920631
Kaquenduri <marchunque7880@gmail.com>
2026-09-14 10:07:20 -0500

    Feat configuracion del programa.cs - appdbcontext

--- Actividad_Caso_2/Program.cs
+++ Actividad_Caso_2/Program.cs
@@ -1,20 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddOpenApi();
+builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+//Reemplazas por vsriable que pusiste al DBCONTEXT
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseExceptionHandler("/Home/Error");
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "api v1");
+        c.RoutePrefix = string.Empty;
+    });
 }
+
 app.UseHttpsRedirection();
-
-
+app.UseRouting();
+app.MapControllers();
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
